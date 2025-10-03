a3948c846947cab7958c4dfa34f8e2a4a057330e
franklinijg-wq <franklinijg@gmail.com>
2025-10-02 23:50:25 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ICMS Pro',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ICMS Pro"),
+      ),
+      body: const Center(
+        child: Text(
+          "Bem-vindo ao Aplicativo ICMS Pro!",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
